c19e640ef840d52ef703a162bdb5cbeba3cc8d08
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-26 16:49:56 +0530

    (Samiul Mamud) Created: Address book system is created

---
+++ src/main/java/com/capgemini/addressbookproblem/AddressBookProblem.java
@@ -0,0 +1,16 @@
+package com.capgemini.addressbookproblem;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+
+public class AddressBookProblem {
+
+	private static final Logger LOG = LogManager.getLogger(AddressBookProblem.class);
+
+	public static void main(String[] args) {
+
+		System.out.println(": Welcome to the Address Book System :");
+		LOG.info("Address book system started");
+	}
+}
