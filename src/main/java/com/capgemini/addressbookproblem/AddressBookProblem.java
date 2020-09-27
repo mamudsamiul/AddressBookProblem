ef4510ba362308568e3be8db42216d585b146cbc
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-28 01:04:29 +0530

    (Samiul Mamud) Modified: Search operation and Modification of addressbook logic is added

--- src/main/java/com/capgemini/addressbookproblem/AddressBookProblem.java
+++ src/main/java/com/capgemini/addressbookproblem/AddressBookProblem.java
@@ -2,45 +2,38 @@ package com.capgemini.addressbookproblem;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookProblem {
 	private static final Logger LOG = LogManager.getLogger(AddressBookProblem.class);
 
 	public static void main(String[] args) {
-		String firstName;
-		String lastName;
-		String address;
-		String city;
-		String state;
-		String zip;
-		String phoneNo;
-		String emailAddress;
+		int flag;
 		System.out.println(": Welcome to the Address Book System :");
 		LOG.info("Address book system started");
-		ArrayList<String> show;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter your First name :");
-		firstName = scan.nextLine();
-		System.out.println("Please enter your Last name :");
-		lastName = scan.nextLine();
-		System.out.println("Please enter your Address :");
-		address = scan.nextLine();
-		System.out.println("Please enter your City :");
-		city = scan.nextLine();
-		System.out.println("Please enter your State:");
-		state = scan.nextLine();
-		System.out.println("Please enter your Zip Code :");
-		zip = scan.nextLine();
-		System.out.println("Please enter your Phone number :");
-		phoneNo = scan.nextLine();
-		System.out.println("Please enter your email Address :");
-		emailAddress = scan.nextLine();
-		scan.close();
-		DataAddToAddressBook firstEntry = new DataAddToAddressBook(firstName, lastName, address, city, state, zip,
-				phoneNo, emailAddress);
-		show = firstEntry.data();
-		System.out.println(show);
+		DataAddToAddressBook firstEntry = UserInputTaking.read();
+		System.out.println(firstEntry.addressBook);
+		flag = NameCheckForUpdate.InsertOrUpdate();
+		System.out.println(flag);
+		switch (flag) {
+		case 1:
+			Scanner scan = new Scanner(System.in);
+			String checkFirstName;
+			String checkLastName;
+			System.out.println("Enter first name:");
+			checkFirstName = scan.nextLine();
+			System.out.println("Enter last name:");
+			checkLastName = scan.nextLine();
+			ValidateName valid = new ValidateName(checkFirstName, checkLastName);
+			if (valid.check(firstEntry)) {
+				firstEntry = UserInputTaking.read();
+				System.out.println(firstEntry.addressBook);
+			} else
+				System.out.println("Invalid Input!");
+			break;
+		default:
+
+		}
+
 	}
 }
---
+++ src/main/java/com/capgemini/addressbookproblem/UserInputTaking.java
@@ -0,0 +1,39 @@
+package com.capgemini.addressbookproblem;
+
+import java.util.Scanner;
+
+public class UserInputTaking {
+	static String firstName;
+	static String lastName;
+	static String address;
+	static String city;
+	static String state;
+	static String zip;
+	static String phoneNo;
+	static String emailAddress;
+
+	public static DataAddToAddressBook read() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter your First name :");
+		firstName = scan.nextLine();
+		System.out.println("Please enter your Last name :");
+		lastName = scan.nextLine();
+		System.out.println("Please enter your Address :");
+		address = scan.nextLine();
+		System.out.println("Please enter your City :");
+		city = scan.nextLine();
+		System.out.println("Please enter your State:");
+		state = scan.nextLine();
+		System.out.println("Please enter your Zip Code :");
+		zip = scan.nextLine();
+		System.out.println("Please enter your Phone number :");
+		phoneNo = scan.nextLine();
+		System.out.println("Please enter your email Address :");
+		emailAddress = scan.nextLine();
+		DataAddToAddressBook firstEntry = new DataAddToAddressBook(firstName, lastName, address, city, state, zip,
+				phoneNo, emailAddress);
+		firstEntry.data();
+		return firstEntry;
+	}
+
+}
